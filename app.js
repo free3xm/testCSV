a906d5fbad1a56184c8397784a18edd7d1150565
free3 <free3xm@gmail.com>
2020-02-06 19:36:59 +0200

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 const config = require("config");
 const upload = require("express-fileupload");
 const path = require("path");
